40fee10ef94107b66ba0143282efbef7f3664af0
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-04-26 22:04:10 -0400

    Added support for linux

--- examples/list/target/main.cpp
+++ examples/list/target/main.cpp
@@ -23,6 +23,9 @@ Value range = Value(Fn([=] (Value __CABLELANG__PARAMS__) mutable {
 Value l = __CABLELANG__PARAMS__[Value(0)]; Value u = __CABLELANG__PARAMS__[Value(1)]; Value result = Value(Value(L({})));	Value n = l;	while ((n < u).to_bool()) {n+=Value(1);
 result+=Value(Value(L({n})));};	return result;}));
 for (Value x : range(Value(L({Value(0), Value(1000)}))).get_list().unwrap().as_vector()) {println(Value(L({x})));};
+Value(Fn([=] (Value __CABLELANG__PARAMS__) mutable {
+println(Value(L({Value("test")})));return Value();
+}))(Value(L({})));
 
 return 0;
 }
--- makefile
+++ makefile
@@ -4,10 +4,11 @@ build:
 	cd cable; make install
 	cd wire; make install; mv ./wire ../bin/wire; cp -Rf ./project ../bin/project
 
-mac:
+unix:
 	rm -Rf ~/cablelang | true
 	mkdir ~/cablelang | true
-	cp -Rf release/mac ~/cablelang/
-	mv -f ~/cablelang/mac ~/cablelang/bin
+	cp -Rf release/unix ~/cablelang/
+	mv -f ~/cablelang/unix ~/cablelang/bin
+	echo 'export PATH="$$HOME/cablelang/bin:$$PATH"' >> ~/.bashrc
 	echo 'export PATH="$$HOME/cablelang/bin:$$PATH"' >> ~/.profile
 	echo 'export PATH="$$HOME/cablelang/bin:$$PATH"' >> ~/.bash_profile
